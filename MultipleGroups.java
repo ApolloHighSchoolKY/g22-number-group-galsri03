c7b2ade5852225b0142dd965761602da49cef673
galsri03 <162865663+galsri03@users.noreply.github.com>
2025-04-09 13:30:13 +0000

    FRQ

--- MultipleGroups.java
+++ MultipleGroups.java
@@ -1,1 +1,12 @@
+public class MultipleGroups
+{
+    {
+        private List<NumberGroup> groupList;
+    }
 
+
+public boolean contains(int num)
+{
+    return 
+}
+}
--- Range.java
+++ Range.java
@@ -1,1 +1,15 @@
+public class Range extends NumberGroup
+{
 
+    public Range()
+    {
+        private int minimum;
+        private int maximum;
+    }
+
+    public Range(int min, int max)
+    {
+        manimum = min;
+        maximum = max;
+    }
+}
